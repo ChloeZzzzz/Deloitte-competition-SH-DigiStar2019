5d0275486e37ee11cec87cee3ef047505953df73
Yifei Zhu <chloe.zhu170524@gmail.com>
2019-07-18 22:30:40 +1000

    weibo api failed

--- weibo.py
+++ weibo.py
@@ -8,3 +8,15 @@ redirect_url = 'https://api.weibo.com/oauth2/default.html'
 auth = WeiboOauth2(client_key, client_secret, redirect_url)
 
 webbrowser.open_new(auth.authorize_url)
+
+code = input('输入 code: ')
+
+token = auth.auth_access(code)
+
+print(token)
+
+client = WeiboClient(token['access_token'])
+
+result = client.get(suffix='comments/show.json', params={'id': 4318237070487349, 'count': 200, 'page': 1})
+
+print(result)
